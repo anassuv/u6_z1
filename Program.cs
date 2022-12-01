392386c9f7866be1fa6ec4625395581cc5e558a7
anassuv <114801727+anassuv@users.noreply.github.com>
2022-12-02 00:00:03 +0300

    all added

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Z
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.Write("Сколько чисел вы хотите ввести: ");
+            int m = Int32.Parse(Console.ReadLine());
+            int k = 0;
+            for (int i = 0; i < m; i++)
+            {
+                Console.Write("Введите {0}-e число: ", i + 1);
+                int x = Int32.Parse(Console.ReadLine());
+                if (x > 0) k++;                
+            }
+            Console.WriteLine("Чисел больше нуля: {0}", k);
+            Console.ReadKey();
+        }
+    }
+}
